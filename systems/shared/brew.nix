3033be633f9de35c6e6c261cb01364561d4b3e36
Tim Lange <odysnutzer@googlemail.com>
2024-09-23 20:00:19 +0200

    Remove colima

--- systems/shared/brew.nix
+++ systems/shared/brew.nix
@@ -21,7 +21,6 @@
 
     brews = [
       "platformsh-cli"
-      "colima"
       "argocd"
       "dnsmasq"
       "hetzner_k3s"
@@ -34,11 +33,4 @@
       "openlens"
     ];
   };
-
-  system.activationScripts.postUserActivation.text = ''
-      $DRY_RUN_CMD rm -rf $VERBOSE_ARG ~/.colima/default
-      $DRY_RUN_CMD mkdir -p $VERBOSE_ARG ~/.colima/default
-      $DRY_RUN_CMD cp $VERBOSE_ARG ${configs/colima/colima.yaml} ~/.colima/default/colima.yaml
-      $DRY_RUN_CMD chmod 644 $VERBOSE_ARG ~/.colima/default/colima.yaml
-    '';
 }
