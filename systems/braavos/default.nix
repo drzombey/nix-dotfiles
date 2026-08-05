05c202b9e905ac84b4f498e2a6f8d86f672e0897
Tim Lange <t.lange@shopware.com>
2026-08-05 11:37:30 +0200

    braavos: weekly GC (14d), store optimise, limit boot entries to 10

--- systems/braavos/default.nix
+++ systems/braavos/default.nix
@@ -5,6 +5,7 @@
 
   # Bootloader
   boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 10;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.initrd.systemd.enable = true;
@@ -13,6 +14,14 @@
 
   networking.hostName = "braavos";
   networking.networkmanager.enable = true;
+
+  # Automatische Garbage Collection + Store-Deduplizierung
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 14d";
+  };
+  nix.optimise.automatic = true;
 
   # Flakes + Cachix
   nix.settings = {
